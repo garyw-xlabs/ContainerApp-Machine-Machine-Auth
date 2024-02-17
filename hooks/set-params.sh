fe0b867d3fc7654f47b4aeb3ef576a4f22a1680e
Gary Walker <gary.walker@x-labsystems.co.uk>
2024-02-17 18:12:35 +0000

    updated script

--- hooks/set-params.sh
+++ hooks/set-params.sh
@@ -25,7 +25,7 @@ echo "commit_id=$commit_id" >> $GITHUB_OUTPUT
 # get the current tags
 echo "RESOURCE_GROUP: $RESOURCE_GROUP"
 echo "APP_NAME: $APP_NAME"
-tags=$(az containerapp show -g $RESOURCE_GROUP -n $APP_NAME --query tags -o json  | tr -d '\r\n')
+tags=$(az containerapp show -g rg-bluegreen2-test -n ca-ndjhmwywztfiy --query tags -o json  | tr -d '\r\n')
 
 echo $tags
 # get the current production label
